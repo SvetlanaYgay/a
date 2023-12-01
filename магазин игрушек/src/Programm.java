0e58844143b789190a68ad3886139a22dd349432
Светлана Югай <135803951+SvetlanaYgay@users.noreply.github.com>
2023-12-01 15:56:54 +0600

    Add files via upload

---
+++ магазин игрушек/src/Programm.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programm {
+    public static void main(String[] args) {
+
+        List<Toy> listToys = new ArrayList<Toy>();
+
+        //инициализация списка игрушек
+        ToyStore toys = new ToyStore(listToys);
+
+        //инициализация списка призовых игрушек
+        List<Toy> prizeToys = new ArrayList<Toy>();
+
+        // инициализируем меню
+        Menu m = new Menu();
+        MenuAction ma = new MenuAction();
+
+        toys.addToy("Самокат", 2, 2);
+        toys.addToy("Настольная игра 'Поле чудес'", 10, 5);
+        toys.addToy("Настольная игра 'Шахматы'", 15, 5);
+        toys.addToy("Бинокль", 20, 10);
+        toys.addToy("Машинка", 25, 15);
+        toys.addToy("Паровозик", 35, 25);
+        toys.addToy("Скакалка", 50, 30);
+
+
+        // бесконечный цикл по меню, пока не выберем режим = 0
+        while (true) {
+            // вызов меню
+            m.showMenu();
+            // если выбрали режим 0 - выход из программы
+            if (m.getAction() == 0) {
+                System.out.println("Пока!");
+                break;
+            } else {
+                System.out.println("Выбран пункт меню: " + m.getAction());
+                ma.selectAction(m.getAction(), toys, prizeToys);
+            }
+        }
+    }
+}
---
+++ магазин игрушек/src/Toy.java
@@ -0,0 +1,63 @@
+public class Toy {
+    private int idToy;
+    private String nameToy;
+    private int amount;
+    private int chanceFalling;
+
+    public Toy(int id, String name) {
+        this(id, name, 1, 1);
+    }
+
+    public Toy(int id, String name, int amount, int chance) {
+//        if (id == null || id.length() > 10) {
+//            throw new IllegalArgumentException("invalid idToy");
+//        }
+//        if (name == null || name.length() > 30) {
+//            throw new IllegalArgumentException("invalid nameToy");
+//        }
+//        if (amount == null || amount.length() > 10) {
+//            throw new IllegalArgumentException("invalid amount");
+//        }
+//        if (chance == null || chance.length() > 3) {
+//            chance = "100";
+//        }
+
+        this.idToy = id;
+        this.nameToy = name;
+        this.amount = amount;
+        this.chanceFalling = chance;
+    }
+
+    public int getIdToy() {
+        return idToy;
+    }
+
+    public String getNameToy() {
+        return nameToy;
+    }
+
+    public void setNameToy(String name) {
+        this.nameToy = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getChanceFalling() {
+        return chanceFalling;
+    }
+
+    public void setChanceFalling(int chance) {
+        this.chanceFalling = chance;
+    }
+
+    @Override
+    public String toString() {
+        return "id:" + idToy + ", наименование:" + nameToy + ", количество:" + amount + ", шанс выпадения:" + chanceFalling + ";";
+    }
+}
